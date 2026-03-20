6f9b94d2dc2a654b857d50c55e60f548caed80ce
Medvid Oleksandr <samaster010604@gmail.com>
2026-03-20 17:14:33 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import './styles/main.scss';
 import './App.scss';
+import Footer from './components/Footer/Footer';
+import { NavBar } from './components/NavBar/Navbar';
+import Home from './components/Pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Favourite from './components/Favourite/Favourite';
+import Cart from './components/Cart/Cart';
+import Phones from './components/Phones/Phones';
+import Tablets from './components/Tablets/Tablets';
+import Accessories from './components/Accessories/Accessories';
+import NotFound from './components/NotFound/NotFound';
+import CardDetails from './components/CardDetails/CardDetails';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="App">
+      <NavBar />
+      <main className="main-with-fixed-header">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourite />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/phones" element={<Phones />} />
+          <Route path="/tablets" element={<Tablets />} />
+          <Route path="/accessories" element={<Accessories />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/phones/:productId" element={<CardDetails />} />
+          <Route path="/tablets/:productId" element={<CardDetails />} />
+          <Route path="/accessories/:productId" element={<CardDetails />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 };
